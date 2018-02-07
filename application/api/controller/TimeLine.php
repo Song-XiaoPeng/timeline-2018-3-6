8c3f10fea6ad402b66e318176ea3c46ef5c80c58
sonexpeng <sonexpeng@163.com>
2018-02-07 13:56:57 +0800

    init

---
+++ application/api/controller/TimeLine.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/18 0018
+ * Time: 15:51
+ */
+
+namespace app\api\controller;
+
+use think\Exception;
+use think\Db;
+
+/**
+ * 时光机&&时光轴api接口
+ * Class TimeLine
+ * @package app\api\controller
+ */
+class TimeLine
+{
+    /**
+     * 时光机页面：添加数据
+     * @return \think\response\Json
+     */
+    public function addArticle()
+    {
+        try {
+            $data = input();
+            $data['date'] = strtotime(date('Y-m-d', time()));//年月日
+            $data['time'] = time() - strtotime(date('Y-m-d', time()));//时间
+            Db::name('article')->insert($data);
+            return json(msg(0, 'success'));
+        } catch (Exception $e) {
+            return json(msg(1, $e->getMessage()));
+        }
+    }
+
+    /**
+     * 时光轴页面数据
+     * @return \think\response\Json
+     * [
+     * 2012:[
+     *  [],[]
+     * ],
+     * 2013:[
+     *  [],[]
+     * ]
+     * ]
+     */
+    public function getTimeLineData()
+    {
+        try {
+            $data = Db::name('article')->order('date desc,time desc')->where('status', 0)->select();
+            array_walk($data, function (&$v) {
+                $v['year'] = date('Y', $v['date']);
+                $v['month'] = date('m', $v['date']);
+                $v['day'] = date('d', $v['date']);
+                $v['time'] = date('H:i:s', $v['time']);
+            });
+            $yearArr = array_unique(array_column($data, 'year'));
+            $tmp = [];
+            foreach ($data as $k => $v) {
+                if (!array_key_exists($v['year'], $tmp)) {
+                    $tmp[$v['year']] = [];
+                } else {
+                    continue;
+                }
+                foreach ($data as $k1 => $v1) {
+                    if ($k == $k1) {//@!! debug 若忽略掉自身的话，结果会将第一条数据排除在外
+                        //continue;
+                    }
+                    if ($v1['year'] == $v['year']) {
+                        $tmp[$v['year']][] = $v1;
+                    }
+                }
+            }
+
+            $res = [
+                'year' => $yearArr,
+                'data' => $tmp
+            ];
+            return json(msg(0, 'success', $res));
+        } catch (Exception $e) {
+            return json(msg(1, $e->getMessage()));
+        }
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/15 0015
+ * Time: 17:53
+ */
+
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return "<p>欢迎</p><a href='http://sone.timeline.hellobirds.top/time_line/index'>sone的好看的时光轴</a><br /><a href='http://sone.timeline.hellobirds.top/time_line/machine'>sone的时光机</a><br /><a href='http://sone.hellobirds.top'>sone的博客</a>";
+    }
+
+    /**
+     * 时光机
+     * @return \think\response\View
+     */
+    public function timeLineMachine()
+    {
+        return view('time_machine'); //默认渲染文件：当前模块/默认视图目录/当前控制器（小写）/当前操作（小写）.html
+        /*
+            1. 指定模板输出
+            return $this->fetch('edit');
+            2. 表示调用当前控制器下面的edit模板
+            return $this->fetch('member/read');
+            3. 跨模块渲染模板
+            return $this->fetch('admin@member/edit');
+         */
+    }
+
+    /**
+     * 时光轴
+     * @return \think\response\View
+     */
+    public function timeLineAxis()
+    {
+        return view('time_axis');
+    }
+}
